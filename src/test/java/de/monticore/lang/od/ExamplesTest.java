8d5396a7312c9848fe53a3251e7af320ad3a24f8
greifenberg <greifenberg@3ddcf219-3c89-4d16-bbf6-790a9aa18b9b>
2016-02-29 15:00:46 +0000

    initial version of symbol table fix parser generation (order of grammar productions) fix od pom
    git-svn-id: https://sselab.de/lab2/private/svn/MontiCore/trunk/mclang/od@25397 3ddcf219-3c89-4d16-bbf6-790a9aa18b9b

--- src/test/java/de/monticore/lang/od/ExamplesTest.java
+++ src/test/java/de/monticore/lang/od/ExamplesTest.java
@@ -5,68 +5,67 @@
  */
 package de.monticore.lang.od;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Optional;
-
-import org.antlr.v4.runtime.RecognitionException;
-import org.junit.Test;
-
 import de.monticore.lang.od._ast.ASTODCompilationUnit;
 import de.monticore.lang.od._parser.ODParser;
 import de.monticore.lang.od.prettyprint.HierachicalODPrettyPrinterConcreteVisitor;
 import de.monticore.lang.od.prettyprint.ODPrettyPrinterConcreteVisitor;
 import de.monticore.prettyprint.IndentPrinter;
 import de.se_rwth.commons.logging.Log;
+import org.antlr.v4.runtime.RecognitionException;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * This test compares the ASTs of the files in the examples folder with the
  * pretty-printed versions of these files.
- * 
+ *
  * @author Robert Heim, Timo Greifenberg
  */
 public class ExamplesTest {
-  
+
   @Test
   public void testQualifiedLinks() throws RecognitionException, IOException {
     Log.enableFailQuick(false);
     test("src/test/resources/examples/od/QualifiedLinks.od", false);
   }
-  
+
   @Test
   public void testAuctionParticipats() throws RecognitionException, IOException {
     test("src/test/resources/examples/od/AuctionParticipants.od", false);
   }
-  
+
   @Test
   public void testVariants() throws RecognitionException, IOException {
     test("src/test/resources/examples/od/Variants.od", false);
   }
-  
+
   @Test
   public void testStereoWithKeyword() throws RecognitionException, IOException {
     test("src/test/resources/examples/od/StereoWithKeyword.od", false);
   }
-  
+
   @Test
   public void testHierarchicalOd() throws RecognitionException, IOException {
     test("src/test/resources/examples/hierarchical/StandardInnerLink.od", true);
   }
-  
+
   @Test
   public void testHierarchicalOdVariants() throws RecognitionException, IOException {
     test("src/test/resources/examples/hierarchical/InnerLinkVariants.od", true);
   }
-  
+
   @Test
   public void testValueCollection() throws RecognitionException, IOException {
     test("src/test/resources/examples/valuecollections/ValueCollection.od", true);
   }
-  
+
   private void test(String modelName, boolean hierarchical)
       throws RecognitionException, IOException {
     Path model = Paths.get(modelName);
@@ -74,7 +73,7 @@ public class ExamplesTest {
     Optional<ASTODCompilationUnit> odDef = parser.parseODCompilationUnit(model.toString());
     assertFalse(parser.hasErrors());
     assertTrue(odDef.isPresent());
-    
+
     // pretty print the AST
     ODPrettyPrinterConcreteVisitor pp;
     if (hierarchical) {
@@ -83,19 +82,19 @@ public class ExamplesTest {
     else {
       pp = new ODPrettyPrinterConcreteVisitor(new IndentPrinter());
     }
-    
+
     pp.handle(odDef.get());
     String ppResult = pp.getPrinter().getContent();
     System.out.println(ppResult);
-    
+
     // parse the printers content
     Optional<ASTODCompilationUnit> ppOd = parser.parseString_ODCompilationUnit(ppResult);
-    
+
     assertFalse(parser.hasErrors());
     assertTrue(ppOd.isPresent());
-    
+
     // must be equal to original parsed AST.
     assertTrue("pretty printed OD: " + ppResult, odDef.get().deepEquals(ppOd.get()));
-    
+
   }
 }
