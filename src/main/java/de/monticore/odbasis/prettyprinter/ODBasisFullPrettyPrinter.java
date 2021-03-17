355339eee0b284f69a0e58aaa6409628974793bd
hillemacher <hillemacher@se-rwth.de>
2021-03-17 18:40:09 +0100

    fixed ambiguity problems occurring in some pretty printers, added additional full pretty printer

--- src/main/java/de/monticore/odbasis/prettyprinter/ODBasisFullPrettyPrinter.java
+++ src/main/java/de/monticore/odbasis/prettyprinter/ODBasisFullPrettyPrinter.java
@@ -1,60 +1,37 @@
-// (c) https://github.com/MontiCore/monticore
+/*
+ (c) https://github.com/MontiCore/monticore
+ */
 
-package de.monticore.od4report.prettyprinter;
+package de.monticore.odbasis.prettyprinter;
 
-import de.monticore.dateliterals._ast.ASTDateLiteralsNode;
-import de.monticore.dateliterals.prettyprinter.DateLiteralsPrettyPrinter;
 import de.monticore.expressions.prettyprint.CommonExpressionsPrettyPrinter;
 import de.monticore.expressions.prettyprint.ExpressionsBasisPrettyPrinter;
 import de.monticore.literals.mccommonliterals._ast.ASTMCCommonLiteralsNode;
 import de.monticore.literals.prettyprint.MCCommonLiteralsPrettyPrinter;
 import de.monticore.od.prettyprinter.ODFullPrettyPrinter;
-import de.monticore.od4report.OD4ReportMill;
-import de.monticore.od4report._ast.ASTOD4ReportNode;
-import de.monticore.od4report._ast.ASTODReportObject;
-import de.monticore.od4report._visitor.OD4ReportTraverser;
-import de.monticore.odattribute._ast.ASTODAttributeNode;
-import de.monticore.odattribute.prettyprinter.ODAttributePrettyPrinter;
+import de.monticore.odbasis.ODBasisMill;
 import de.monticore.odbasis._ast.ASTODBasisNode;
-import de.monticore.odbasis.prettyprinter.ODBasisPrettyPrinter;
-import de.monticore.odlink._ast.ASTODLink;
-import de.monticore.odlink._ast.ASTODLinkNode;
-import de.monticore.odlink.prettyprinter.ODLinkPrettyPrinter;
+import de.monticore.odbasis._visitor.ODBasisTraverser;
 import de.monticore.prettyprint.IndentPrinter;
 import de.monticore.prettyprint.UMLModifierPrettyPrinter;
 import de.monticore.prettyprint.UMLStereotypePrettyPrinter;
 import de.monticore.types.mcbasictypes._ast.ASTMCBasicTypesNode;
-import de.monticore.types.mccollectiontypes._ast.ASTMCCollectionTypesNode;
 import de.monticore.types.prettyprint.MCBasicTypesPrettyPrinter;
-import de.monticore.types.prettyprint.MCCollectionTypesPrettyPrinter;
 import de.monticore.umlmodifier._ast.ASTUMLModifierNode;
 import de.monticore.umlstereotype._ast.ASTUMLStereotypeNode;
 
-public class OD4ReportFullPrettyPrinter extends ODFullPrettyPrinter {
+public class ODBasisFullPrettyPrinter extends ODFullPrettyPrinter {
 
-  protected OD4ReportTraverser traverser = OD4ReportMill.traverser();
+  protected ODBasisTraverser traverser = ODBasisMill.traverser();
 
-  public OD4ReportFullPrettyPrinter() {
+  public ODBasisFullPrettyPrinter() {
     super();
     init();
   }
 
-  public OD4ReportFullPrettyPrinter(IndentPrinter printer) {
+  public ODBasisFullPrettyPrinter(IndentPrinter printer) {
     super(printer);
     init();
-  }
-
-  public String prettyprint(ASTOD4ReportNode astod4ReportNode) {
-    getPrinter().clearBuffer();
-    astod4ReportNode.accept(traverser);
-    return getPrinter().getContent();
-  }
-
-  // has to be done due to ambiguity issues
-  public String prettyprint(ASTODReportObject astodReportObject) {
-    getPrinter().clearBuffer();
-    astodReportObject.accept(traverser);
-    return getPrinter().getContent();
   }
 
   public String prettyprint(ASTODBasisNode astodBasisNode) {
@@ -63,40 +40,9 @@ public class OD4ReportFullPrettyPrinter extends ODFullPrettyPrinter {
     return getPrinter().getContent();
   }
 
-  public String prettyprint(ASTODLinkNode astodLinkNode) {
-    getPrinter().clearBuffer();
-    astodLinkNode.accept(traverser);
-    return getPrinter().getContent();
-  }
-
-  // has to be done due to ambiguity issues
-  public String prettyprint(ASTODLink astodLink) {
-    getPrinter().clearBuffer();
-    astodLink.accept(traverser);
-    return getPrinter().getContent();
-  }
-
-  public String prettyprint(ASTODAttributeNode ASTODAttributeNode) {
-    getPrinter().clearBuffer();
-    ASTODAttributeNode.accept(traverser);
-    return getPrinter().getContent();
-  }
-
-  public String prettyprint(ASTDateLiteralsNode astDateLiteralsNode) {
-    getPrinter().clearBuffer();
-    astDateLiteralsNode.accept(traverser);
-    return getPrinter().getContent();
-  }
-
   public String prettyprint(ASTMCBasicTypesNode astmcType) {
     getPrinter().clearBuffer();
     astmcType.accept(traverser);
-    return getPrinter().getContent();
-  }
-
-  public String prettyprint(ASTMCCollectionTypesNode astmcCollectionTypesNode) {
-    getPrinter().clearBuffer();
-    astmcCollectionTypesNode.accept(traverser);
     return getPrinter().getContent();
   }
 
@@ -111,7 +57,7 @@ public class OD4ReportFullPrettyPrinter extends ODFullPrettyPrinter {
     astumlModifierNode.accept(traverser);
     return getPrinter().getContent();
   }
-  
+
   public String prettyprint(ASTMCCommonLiteralsNode astmcCommonLiteralsNode) {
     getPrinter().clearBuffer();
     astmcCommonLiteralsNode.accept(traverser);
@@ -121,26 +67,22 @@ public class OD4ReportFullPrettyPrinter extends ODFullPrettyPrinter {
   protected void init() {
     // ods
     traverser.setODBasisHandler(new ODBasisPrettyPrinter(printer));
-    traverser.setOD4ReportHandler(new OD4ReportPrettyPrinter(printer));
-    traverser.setODLinkHandler(new ODLinkPrettyPrinter(printer));
-    traverser.setODAttributeHandler(new ODAttributePrettyPrinter(printer));
 
-    // date literals
-    traverser.setDateLiteralsHandler(new DateLiteralsPrettyPrinter(printer));
-
+    // mccommons
     MCCommonLiteralsPrettyPrinter commonLiterals = new MCCommonLiteralsPrettyPrinter(printer);
     traverser.add4MCCommonLiterals(commonLiterals);
     traverser.setMCCommonLiteralsHandler(commonLiterals);
-    
+
     // uml modifier
     UMLModifierPrettyPrinter umlModifier = new UMLModifierPrettyPrinter(printer);
     traverser.add4UMLModifier(umlModifier);
     traverser.setUMLModifierHandler(umlModifier);
-    
+
+    // uml stereotype
     UMLStereotypePrettyPrinter umlStereotype = new UMLStereotypePrettyPrinter(printer);
     traverser.add4UMLStereotype(umlStereotype);
     traverser.setUMLStereotypeHandler(umlStereotype);
-    
+
     // common expressions
     CommonExpressionsPrettyPrinter commonExpressions = new CommonExpressionsPrettyPrinter(printer);
     traverser.setCommonExpressionsHandler(commonExpressions);
@@ -148,15 +90,11 @@ public class OD4ReportFullPrettyPrinter extends ODFullPrettyPrinter {
     ExpressionsBasisPrettyPrinter basicExpression = new ExpressionsBasisPrettyPrinter(printer);
     traverser.setExpressionsBasisHandler(basicExpression);
     traverser.add4ExpressionsBasis(basicExpression);
-    
-    // collection types
-    MCCollectionTypesPrettyPrinter collectionTypes = new MCCollectionTypesPrettyPrinter(printer);
-    traverser.setMCCollectionTypesHandler(collectionTypes);
-    traverser.add4MCCollectionTypes(collectionTypes);
-    
+
+    // mc basictypes
     MCBasicTypesPrettyPrinter basicTypes = new MCBasicTypesPrettyPrinter(printer);
     traverser.setMCBasicTypesHandler(basicTypes);
     traverser.add4MCBasicTypes(basicTypes);
   }
-  
+
 }
--- src/main/java/de/monticore/odlink/prettyprinter/ODLinkFullPrettyPrinter.java
+++ src/main/java/de/monticore/odlink/prettyprinter/ODLinkFullPrettyPrinter.java
@@ -1,65 +1,45 @@
-// (c) https://github.com/MontiCore/monticore
+/*
+ (c) https://github.com/MontiCore/monticore
+ */
 
-package de.monticore.od4report.prettyprinter;
+package de.monticore.odlink.prettyprinter;
 
-import de.monticore.dateliterals._ast.ASTDateLiteralsNode;
-import de.monticore.dateliterals.prettyprinter.DateLiteralsPrettyPrinter;
 import de.monticore.expressions.prettyprint.CommonExpressionsPrettyPrinter;
 import de.monticore.expressions.prettyprint.ExpressionsBasisPrettyPrinter;
 import de.monticore.literals.mccommonliterals._ast.ASTMCCommonLiteralsNode;
 import de.monticore.literals.prettyprint.MCCommonLiteralsPrettyPrinter;
 import de.monticore.od.prettyprinter.ODFullPrettyPrinter;
-import de.monticore.od4report.OD4ReportMill;
-import de.monticore.od4report._ast.ASTOD4ReportNode;
-import de.monticore.od4report._ast.ASTODReportObject;
-import de.monticore.od4report._visitor.OD4ReportTraverser;
-import de.monticore.odattribute._ast.ASTODAttributeNode;
-import de.monticore.odattribute.prettyprinter.ODAttributePrettyPrinter;
 import de.monticore.odbasis._ast.ASTODBasisNode;
 import de.monticore.odbasis.prettyprinter.ODBasisPrettyPrinter;
+import de.monticore.odlink.ODLinkMill;
 import de.monticore.odlink._ast.ASTODLink;
 import de.monticore.odlink._ast.ASTODLinkNode;
-import de.monticore.odlink.prettyprinter.ODLinkPrettyPrinter;
+import de.monticore.odlink._visitor.ODLinkTraverser;
 import de.monticore.prettyprint.IndentPrinter;
 import de.monticore.prettyprint.UMLModifierPrettyPrinter;
 import de.monticore.prettyprint.UMLStereotypePrettyPrinter;
 import de.monticore.types.mcbasictypes._ast.ASTMCBasicTypesNode;
-import de.monticore.types.mccollectiontypes._ast.ASTMCCollectionTypesNode;
 import de.monticore.types.prettyprint.MCBasicTypesPrettyPrinter;
-import de.monticore.types.prettyprint.MCCollectionTypesPrettyPrinter;
 import de.monticore.umlmodifier._ast.ASTUMLModifierNode;
 import de.monticore.umlstereotype._ast.ASTUMLStereotypeNode;
 
-public class OD4ReportFullPrettyPrinter extends ODFullPrettyPrinter {
+public class ODLinkFullPrettyPrinter extends ODFullPrettyPrinter {
 
-  protected OD4ReportTraverser traverser = OD4ReportMill.traverser();
+  protected ODLinkTraverser traverser = ODLinkMill.traverser();
 
-  public OD4ReportFullPrettyPrinter() {
+  public ODLinkFullPrettyPrinter() {
     super();
     init();
   }
 
-  public OD4ReportFullPrettyPrinter(IndentPrinter printer) {
+  public ODLinkFullPrettyPrinter(IndentPrinter printer) {
     super(printer);
     init();
   }
 
-  public String prettyprint(ASTOD4ReportNode astod4ReportNode) {
+  public String prettyprint(ASTODLink astodLink) {
     getPrinter().clearBuffer();
-    astod4ReportNode.accept(traverser);
-    return getPrinter().getContent();
-  }
-
-  // has to be done due to ambiguity issues
-  public String prettyprint(ASTODReportObject astodReportObject) {
-    getPrinter().clearBuffer();
-    astodReportObject.accept(traverser);
-    return getPrinter().getContent();
-  }
-
-  public String prettyprint(ASTODBasisNode astodBasisNode) {
-    getPrinter().clearBuffer();
-    astodBasisNode.accept(traverser);
+    astodLink.accept(traverser);
     return getPrinter().getContent();
   }
 
@@ -69,34 +49,15 @@ public class OD4ReportFullPrettyPrinter extends ODFullPrettyPrinter {
     return getPrinter().getContent();
   }
 
-  // has to be done due to ambiguity issues
-  public String prettyprint(ASTODLink astodLink) {
+  public String prettyprint(ASTODBasisNode astodBasisNode) {
     getPrinter().clearBuffer();
-    astodLink.accept(traverser);
-    return getPrinter().getContent();
-  }
-
-  public String prettyprint(ASTODAttributeNode ASTODAttributeNode) {
-    getPrinter().clearBuffer();
-    ASTODAttributeNode.accept(traverser);
-    return getPrinter().getContent();
-  }
-
-  public String prettyprint(ASTDateLiteralsNode astDateLiteralsNode) {
-    getPrinter().clearBuffer();
-    astDateLiteralsNode.accept(traverser);
+    astodBasisNode.accept(traverser);
     return getPrinter().getContent();
   }
 
   public String prettyprint(ASTMCBasicTypesNode astmcType) {
     getPrinter().clearBuffer();
     astmcType.accept(traverser);
-    return getPrinter().getContent();
-  }
-
-  public String prettyprint(ASTMCCollectionTypesNode astmcCollectionTypesNode) {
-    getPrinter().clearBuffer();
-    astmcCollectionTypesNode.accept(traverser);
     return getPrinter().getContent();
   }
 
@@ -111,7 +72,7 @@ public class OD4ReportFullPrettyPrinter extends ODFullPrettyPrinter {
     astumlModifierNode.accept(traverser);
     return getPrinter().getContent();
   }
-  
+
   public String prettyprint(ASTMCCommonLiteralsNode astmcCommonLiteralsNode) {
     getPrinter().clearBuffer();
     astmcCommonLiteralsNode.accept(traverser);
@@ -121,26 +82,23 @@ public class OD4ReportFullPrettyPrinter extends ODFullPrettyPrinter {
   protected void init() {
     // ods
     traverser.setODBasisHandler(new ODBasisPrettyPrinter(printer));
-    traverser.setOD4ReportHandler(new OD4ReportPrettyPrinter(printer));
     traverser.setODLinkHandler(new ODLinkPrettyPrinter(printer));
-    traverser.setODAttributeHandler(new ODAttributePrettyPrinter(printer));
 
-    // date literals
-    traverser.setDateLiteralsHandler(new DateLiteralsPrettyPrinter(printer));
-
+    // mccommons
     MCCommonLiteralsPrettyPrinter commonLiterals = new MCCommonLiteralsPrettyPrinter(printer);
     traverser.add4MCCommonLiterals(commonLiterals);
     traverser.setMCCommonLiteralsHandler(commonLiterals);
-    
+
     // uml modifier
     UMLModifierPrettyPrinter umlModifier = new UMLModifierPrettyPrinter(printer);
     traverser.add4UMLModifier(umlModifier);
     traverser.setUMLModifierHandler(umlModifier);
-    
+
+    // uml stereotype
     UMLStereotypePrettyPrinter umlStereotype = new UMLStereotypePrettyPrinter(printer);
     traverser.add4UMLStereotype(umlStereotype);
     traverser.setUMLStereotypeHandler(umlStereotype);
-    
+
     // common expressions
     CommonExpressionsPrettyPrinter commonExpressions = new CommonExpressionsPrettyPrinter(printer);
     traverser.setCommonExpressionsHandler(commonExpressions);
@@ -148,15 +106,11 @@ public class OD4ReportFullPrettyPrinter extends ODFullPrettyPrinter {
     ExpressionsBasisPrettyPrinter basicExpression = new ExpressionsBasisPrettyPrinter(printer);
     traverser.setExpressionsBasisHandler(basicExpression);
     traverser.add4ExpressionsBasis(basicExpression);
-    
-    // collection types
-    MCCollectionTypesPrettyPrinter collectionTypes = new MCCollectionTypesPrettyPrinter(printer);
-    traverser.setMCCollectionTypesHandler(collectionTypes);
-    traverser.add4MCCollectionTypes(collectionTypes);
-    
+
+    // mc basictypes
     MCBasicTypesPrettyPrinter basicTypes = new MCBasicTypesPrettyPrinter(printer);
     traverser.setMCBasicTypesHandler(basicTypes);
     traverser.add4MCBasicTypes(basicTypes);
   }
-  
+
 }
