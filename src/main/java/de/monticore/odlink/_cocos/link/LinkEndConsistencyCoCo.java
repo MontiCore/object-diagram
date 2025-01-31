2507d5aa6a54bb8a0ab4a89b1bd4f2fffae51d8f
Janik Rapp <rapp@se-rwth.de>
2025-01-31 14:32:41 +0100

    reimplemented LinkEndConsistencyCoCo

--- src/main/java/de/monticore/odlink/_cocos/link/LinkEndConsistencyCoCo.java
+++ src/main/java/de/monticore/odlink/_cocos/link/LinkEndConsistencyCoCo.java
@@ -1,12 +1,11 @@
 /* (c) https://github.com/MontiCore/monticore */
 
-
 package de.monticore.odlink._cocos.link;
 
 import de.monticore.odlink._ast.ASTODLink;
 import de.monticore.odlink._cocos.ODLinkASTODLinkCoCo;
-import de.monticore.symbols.basicsymbols._symboltable.BasicSymbolsSymbols2Json;
 import de.monticore.symbols.basicsymbols._symboltable.VariableSymbol;
+import de.monticore.types.check.SymTypeExpression;
 import de.se_rwth.commons.logging.Log;
 
 import java.util.List;
@@ -18,52 +17,51 @@ import java.util.stream.Collectors;
  * of the definition
  */
 public class LinkEndConsistencyCoCo implements ODLinkASTODLinkCoCo {
-
+  
   @Override
   public void check(ASTODLink node) {
     List<VariableSymbol> leftSymbols = node.getLeftReferenceNames().stream().map(refName -> {
       Optional<VariableSymbol> symbol = node.getEnclosingScope().resolveVariable(refName);
-      if (!symbol.isPresent()) {
-        Log.error("Violation of CoCo 'LinkEndConsistencyCoCo'", node.get_SourcePositionStart());
+      if (symbol.isEmpty()) {
+        Log.error("0x0D008: Violation of CoCo 'LinkEndConsistencyCoCo'",
+            node.get_SourcePositionStart());
       }
       return symbol.get();
     }).collect(Collectors.toList());
-
-    /* TODO SH: Is there any nicer way to check equality? */
-    BasicSymbolsSymbols2Json tablePrinter = new BasicSymbolsSymbols2Json();
-    tablePrinter.visit(leftSymbols.get(0));
-    String firstTypeJSON = tablePrinter.getSerializedString();
-    for (VariableSymbol sym : leftSymbols) {
-      tablePrinter = new BasicSymbolsSymbols2Json();
-      tablePrinter.visit(sym);
-      String typeJSON = tablePrinter.getSerializedString();
-
-      if (!firstTypeJSON.equals(typeJSON)) {
-        Log.error("Violation of CoCo 'LinkEndConsistencyCoCo'", node.get_SourcePositionStart());
-      }
+    
+    if (hasMultipleTypes(leftSymbols)) {
+      Log.error("0x0D008: Violation of CoCo 'LinkEndConsistencyCoCo'",
+          node.get_SourcePositionStart());
     }
-
-    /* TODO SH: Is there any nicer way to check equality? */
+    
     List<VariableSymbol> rightSymbols = node.getRightReferenceNames().stream().map(refName -> {
       Optional<VariableSymbol> symbol = node.getEnclosingScope().resolveVariable(refName);
-      if (!symbol.isPresent()) {
-        Log.error("Violation of CoCo 'LinkEndConsistencyCoCo'", node.get_SourcePositionStart());
+      if (symbol.isEmpty()) {
+        Log.error("0x0D008: Violation of CoCo 'LinkEndConsistencyCoCo'",
+            node.get_SourcePositionStart());
       }
       return symbol.get();
     }).collect(Collectors.toList());
-
-    tablePrinter = new BasicSymbolsSymbols2Json();
-    tablePrinter.visit(rightSymbols.get(0));
-    firstTypeJSON = tablePrinter.getSerializedString();
-    for (VariableSymbol sym : rightSymbols) {
-      tablePrinter = new BasicSymbolsSymbols2Json();
-      tablePrinter.visit(sym);
-      String typeJSON = tablePrinter.getSerializedString();
-
-      if (!firstTypeJSON.equals(typeJSON)) {
-        Log.error("Violation of CoCo 'LinkEndConsistencyCoCo'", node.get_SourcePositionStart());
-      }
+    
+    if (hasMultipleTypes(rightSymbols)) {
+      Log.error("0x0D008: Violation of CoCo 'LinkEndConsistencyCoCo'",
+          node.get_SourcePositionStart());
     }
   }
-
+  
+  protected boolean hasMultipleTypes(List<VariableSymbol> symbols) {
+    if (symbols.isEmpty()) {
+      return false;
+    }
+    VariableSymbol firstSymbol = symbols.get(0);
+    SymTypeExpression firstSymbolType = firstSymbol.getType();
+    for (VariableSymbol symbol : symbols) {
+      SymTypeExpression symbolType = symbol.getType();
+      if (!firstSymbolType.deepEquals(symbolType)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
 }
