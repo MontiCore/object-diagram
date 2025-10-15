344e512bb6fe77a1c74c8bd4d1f1b8f2227a4c81
Hendrik7889 <44064629+Hendrik7889@users.noreply.github.com>
2025-10-15 18:35:52 +0200

    replaced Log::errorIfNull and Objects::requireNonNull with their respective Guava implementation

--- src/main/java/de/monticore/od4development/_symboltable/OD4DevelopmentScopesGenitor.java
+++ src/main/java/de/monticore/od4development/_symboltable/OD4DevelopmentScopesGenitor.java
@@ -1,6 +1,7 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.od4development._symboltable;
 
+import com.google.common.base.Preconditions;
 import de.monticore.odbasis._ast.ASTODArtifact;
 import de.monticore.symboltable.ImportStatement;
 import de.se_rwth.commons.logging.Log;
@@ -10,7 +11,7 @@ import java.util.ArrayList;
 public class OD4DevelopmentScopesGenitor extends OD4DevelopmentScopesGenitorTOP {
 
   public IOD4DevelopmentArtifactScope createFromAST (ASTODArtifact rootNode) {
-    Log.errorIfNull(rootNode, "0xA7004x78995 Error by creating of the OD4DevelopmentScopesGenitor symbol table: top ast node is null");
+    Preconditions.checkNotNull(rootNode, "0xA7004x78995 Error by creating of the OD4DevelopmentScopesGenitor symbol table: top ast node is null");
     IOD4DevelopmentArtifactScope artifactScope = de.monticore.od4development.OD4DevelopmentMill.artifactScope();
     
     // set package
