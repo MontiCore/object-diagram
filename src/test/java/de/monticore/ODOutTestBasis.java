a4944fdb18a43830115e1f036ed393c7b7413415
Janik Rapp <rapp@se-rwth.de>
2026-07-15 11:33:55 +0200

    add javadoc

--- src/test/java/de/monticore/ODOutTestBasis.java
+++ src/test/java/de/monticore/ODOutTestBasis.java
@@ -9,9 +9,15 @@ import java.io.PrintStream;
 
 public abstract class ODOutTestBasis extends ODTestBasis {
   
+  /** Captures everything written to {@code System.out} during tests. */
   protected static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+  /** Captures everything written to {@code System.err} during tests. */
   protected static final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
   
+  /**
+   * Redirects standard output and error streams to in-memory buffers once per test class.
+   */
   @BeforeAll
   public static void redirectStreams() {
     System.setOut(new PrintStream(outContent));
@@ -20,16 +26,29 @@ public abstract class ODOutTestBasis extends ODTestBasis {
     errContent.reset();
   }
   
+  /**
+   * Clears captured output after each test to avoid cross-test interference.
+   */
   @AfterEach
   public void resetStreams() {
     outContent.reset();
     errContent.reset();
   }
   
+  /**
+   * Returns the currently captured standard output.
+   *
+   * @return captured {@code System.out} content as a string
+   */
   protected String getOut() {
     return outContent.toString();
   }
   
+  /**
+   * Returns the currently captured error output.
+   *
+   * @return captured {@code System.err} content as a string
+   */
   protected String getErr() {
     return errContent.toString();
   }
