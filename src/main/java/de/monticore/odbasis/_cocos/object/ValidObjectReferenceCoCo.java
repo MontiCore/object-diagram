17460ae0e811c8f30b5f3355d23afd102759dd25
Janik Rapp <rapp@se-rwth.de>
2026-07-20 12:00:24 +0200

    refactor ValidObjectReferenceCoCo and add tests

--- src/main/java/de/monticore/odbasis/_cocos/object/ValidObjectReferenceCoCo.java
+++ src/main/java/de/monticore/odbasis/_cocos/object/ValidObjectReferenceCoCo.java
@@ -12,20 +12,29 @@ import de.se_rwth.commons.logging.Log;
 
 import java.util.Optional;
 
+/**
+ * Checks that every object attribute reference used inside an {@link ASTODObject} resolves to a
+ * variable that is available in the enclosing scope of that object.
+ * <p>
+ * The CoCo inspects simple attribute values whose expression is a name expression. If such a name
+ * cannot be resolved to a {@link VariableSymbol}, the attribute reference is considered invalid
+ * and an error is reported at the start position of the object.
+ */
 public class ValidObjectReferenceCoCo implements ODBasisASTODObjectCoCo {
+
+  public static final String ERROR_UNRESOLVED_OBJECT_REFERENCE =
+      "0x0D001: The referenced object attribute '%s' cannot be resolved in the enclosing scope.";
 
   @Override
   public void check(ASTODObject node) {
     for (ASTODAttribute astodAttribute : node.getODAttributeList()) {
       if (astodAttribute.isPresentODValue()) {
         ASTODValue astodValue = astodAttribute.getODValue();
-        if (astodValue instanceof ASTODSimpleAttributeValue) {
-          ASTODSimpleAttributeValue simpleAttributeValue = (ASTODSimpleAttributeValue) astodValue;
-          if (simpleAttributeValue.getExpression() instanceof ASTNameExpression && !this
-              .checkReference((ASTNameExpression) simpleAttributeValue.getExpression(), node)) {
-            Log.error("0x0D001: Violation of CoCo 'ValidObjectReferenceCoCo'",
-                node.get_SourcePositionStart());
-          }
+        if (astodValue instanceof ASTODSimpleAttributeValue simpleAttributeValue
+            && simpleAttributeValue.getExpression() instanceof ASTNameExpression astNameExpression
+            && !this.checkReference(astNameExpression, node)) {
+          Log.error(String.format(ERROR_UNRESOLVED_OBJECT_REFERENCE, astNameExpression.getName()),
+              node.get_SourcePositionStart());
         }
       }
     }
