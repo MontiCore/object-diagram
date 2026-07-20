c38b6b0dcb0026a734ec99f0f21afe7ea5cf1846
Janik Rapp <rapp@se-rwth.de>
2026-07-20 11:23:32 +0200

    refactor NoAbstractAttributesCoCo and add tests

--- src/main/java/de/monticore/odbasis/_cocos/attributes/NoAbstractAttributesCoCo.java
+++ src/main/java/de/monticore/odbasis/_cocos/attributes/NoAbstractAttributesCoCo.java
@@ -8,15 +8,22 @@ import de.monticore.umlmodifier._ast.ASTModifier;
 import de.se_rwth.commons.logging.Log;
 
 /**
- * Attributes shouldn't be abstract.
+ * Checks that an {@link ASTODAttribute} is not declared with the {@code abstract} modifier.
+ * <p>
+ * Object diagram attributes represent concrete attribute values or declarations of objects.
+ * Therefore, an abstract modifier is not allowed on such attributes. If an attribute is declared
+ * as abstract, an error is reported at the start position of that attribute.
  */
 public class NoAbstractAttributesCoCo implements ODBasisASTODAttributeCoCo {
+
+  public static final String ERROR_ABSTRACT_ATTRIBUTE =
+      "0x0D005: An object diagram attribute must not be declared abstract.";
 
   @Override
   public void check(ASTODAttribute node) {
     ASTModifier attrModifier = node.getModifier();
     if (attrModifier.isAbstract()) {
-      Log.error("0x0D005: Violation of CoCo 'NoAbstractAttributesCoCo'", node.get_SourcePositionStart());
+      Log.error(ERROR_ABSTRACT_ATTRIBUTE, node.get_SourcePositionStart());
     }
   }
 
